bab822a9828fda3acda93db0ddbcd4eff055f7f8
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-01 11:44:24 +0500

    koo

---
+++ src/app/blogs/page.jsx
@@ -0,0 +1,13 @@
+// src/app/blogs/page.jsx
+"use client";
+
+import React from "react";
+import Portfolio from "@/components/home/portfolio";
+
+export default function BlogsPage() {
+  return (
+    <div>
+      <Portfolio />
+    </div>
+  );
+}
